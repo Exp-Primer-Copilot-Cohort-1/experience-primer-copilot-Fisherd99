15ae590a9f84a57ff25e0e03fc2fd4d5949e7537
Ziqing Guan <82786270+Fisherd99@users.noreply.github.com>
2024-05-15 05:22:28 +0000

    commrnt

--- comments.js
+++ comments.js
@@ -1,30 +1,9 @@
 //create a web server
-var http = require('http');
-
-//create a web server
-var server = http.createServer(function(req, res) {
-    //get the comments module
-    var comments = require('./comments');
-    //set the response content type
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    //send the comments to the client
-    res.end(JSON.stringify(comments.getComments()));
-});
-
-//listen on port 3000
-server.listen(3000);
-
-console.log('Server running at http://localhost:3000/');
-
-// Path: comments.js
-//comments module
-var comments = [
-    { name: 'John', comment: 'Hello!' },
-    { name: 'Jane', comment: 'Hi!' },
-    { name: 'Jack', comment: 'Goodbye!' }
-];
-
-//export the comments array
-exports.getComments = function() {
-    return comments;
-};
+//create a request handler
+//create a route handler
+//create a route handler for the comments page
+//create a route handler for the comments post request
+//create a route handler for the comments delete request
+//create a route handler for the comments put request
+//create a route handler for the comments patch request
+//create a route handler for the comments get request
